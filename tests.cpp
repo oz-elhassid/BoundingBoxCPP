3a689448c2019036936a56e65d772f6a3d57f493
oz <oz.elhassid@gmail.com>
2020-04-20 12:49:21 +0300

    better tests explanation

--- tests.cpp
+++ tests.cpp
@@ -16,12 +16,14 @@ int main()
     const BoundingBox boxE = BoundingBox(150, 190.5, 190.5, 150.5);
     vector<BoundingBox> arr = {boxA, boxB, boxC, boxD, boxE, boxB, boxD};
 
-    cout<<(BoundingBox::checkRelation(boxA, boxB))<<endl;
-    cout<<(BoundingBox::checkRelation(boxA, boxC))<<endl;
-    cout<<(BoundingBox::checkRelation(boxD, boxE))<<endl;
+    cout<<("boxA and boxB: " + BoundingBox::checkRelation(boxA, boxB))<<endl;
+    cout<<("boxA and boxC: " + BoundingBox::checkRelation(boxA, boxC))<<endl;
+    cout<<("boxD and boxE: " + BoundingBox::checkRelation(boxD, boxE))<<endl;
     vector<BoundingBox> checked1 = BoundingBox::checkArray(arr);
+    cout<<("a separate array created from arr:")<<endl;
     print(checked1);
-    cout<<(BoundingBox::checkRelation(boxD, boxE, BoundingBox::boxRelation::inside))<<endl;
+    cout<<("boxD and boxE not assuming a box in a box is separate: " + BoundingBox::checkRelation(boxD, boxE, BoundingBox::boxRelation::inside))<<endl;
     vector<BoundingBox> checked2 = BoundingBox::checkArray(arr, BoundingBox::boxRelation::inside);
+    cout<<("a separate array created from arr not assuming a box in a box is separate:")<<endl;
     print(checked2);
 }
